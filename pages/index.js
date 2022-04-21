646eb17e7ae37e051ffabd29171f13426bb2ae42
Anthony <seaflyscontact@gmail.com>
2022-04-21 15:13:35 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
@@ -11,8 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
+      <Navbar/>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="underline text-3xl">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
