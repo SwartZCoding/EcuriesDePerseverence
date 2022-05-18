2d0b3c486a0ed218cf4b5f0c6c0bf30db3528695
Anthony <seaflyscontact@gmail.com>
2022-05-18 14:59:55 +0200

    :art: Update pensionnaire & user pages

--- frontend/pages/admin/users.js
+++ frontend/pages/admin/users.js
@@ -49,60 +49,20 @@ function Pensionnaire() {
       <GridItem xs={12} sm={12} md={12}>
         <Card>
           <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Simple Table</h4>
-            <p className={classes.cardCategoryWhite}>
-              Here is a subtitle for this table
-            </p>
+            <h4 className={classes.cardTitleWhite}>Liste des Utilisateurs</h4>
+            <button className={""}>Ajouter un utilisateur</button>
           </CardHeader>
           <CardBody>
             <Table
               tableHeaderColor="primary"
-              tableHead={["Name", "Country", "City", "Salary"]}
+              tableHead={["Nom", "Prénom", "Rôle", "Email", "Numéro de téléphone"]}
               tableData={[
-                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                ["Mason Porter", "Chile", "Gloucester", "$78,615"],
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card plain>
-          <CardHeader plain color="primary">
-            <h4 className={classes.cardTitleWhite}>
-              Table on Plain Background
-            </h4>
-            <p className={classes.cardCategoryWhite}>
-              Here is a subtitle for this table
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["ID", "Name", "Country", "City", "Salary"]}
-              tableData={[
-                ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
-                ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
-                ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
-                [
-                  "4",
-                  "Philip Chaney",
-                  "$38,735",
-                  "Korea, South",
-                  "Overland Park",
-                ],
-                [
-                  "5",
-                  "Doris Greene",
-                  "$63,542",
-                  "Malawi",
-                  "Feldkirchen in Kärnten",
-                ],
-                ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"],
+                ["Dakota Rice", "Niger", "Admin", "test@gmail.com", "0606060606"],
+                ["Minerva Hooper", "Curaçao", "Professionnel", "test@gmail.com", "0606060606"],
+                ["Sage Rodriguez", "Netherlands", "Professionnel", "test@gmail.com", "0606060606"],
+                ["Philip Chaney", "Korea, South", "Utilisateur", "test@gmail.com", "0606060606"],
+                ["Doris Greene", "Malawi", "Utilisateur", "test@gmail.com", "0606060606"],
+                ["Mason Porter", "Chile", "Utilisateur", "test@gmail.com", "0606060606"],
               ]}
             />
           </CardBody>
